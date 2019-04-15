2b9516a33049384f92bdd2b3ece2cfe241f16e83
Nick Lee <leexnick@gmail.com>
2019-04-15 09:36:24 -0700

    reactified

--- index.js
+++ index.js
@@ -2,20 +2,19 @@
 
 /* globals ReactDOM */
 
-function App() {
+function App(props) {
   return (
-    <div>
-      <header role='header'>
-        <h1>Sally Student Resume</h1>
-      </header>
+    <div className="container">
+      <Header name={props.name} />
 
       <main>
         <section role='region'>
           <address role='contact-info'>
-            <p>Name: </p>
-            <p>Email: </p>
-            <p>Phone Number: </p>
-            <p>Address: </p>
+            <img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" alt="Picutre of Sally with crossed arms" />
+            <p>Name: {props.name}</p>
+            <p>Email: sallystudent@gmail.com</p>
+            <p>Phone Number: 123.456.7890</p>
+            <p>Address: 123 Nowhere St., Los Angeles, CA 90001</p>
           </address>
         </section>
 
@@ -53,8 +52,12 @@ function App() {
   )
 }
 
+App.defaultProps = {
+  name: 'Sally Student'
+};
+
 
 ReactDOM.render(
-  <App />,
+  <App name="Jimmy Smith"/>,
   document.querySelector('#root')
 );
